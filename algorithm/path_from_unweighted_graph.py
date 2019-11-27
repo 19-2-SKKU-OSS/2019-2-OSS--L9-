5ab941b851e026bbf22656d48d44818aab42ef96
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2019-11-27 21:05:24 +0900

    [ADD] Create Path_from_unweighted_graph.py

---
+++ algorithm/path_from_unweighted_graph.py
@@ -0,0 +1,11 @@
+'''
+Given an unweighted directional graph G, write a program to find out whether there is a path from i to j for all vertices (i, j).
+
+The first line gives the number of vertices N (1 ≤ N ≤ 100). Lines 2 through N give the adjacency matrix of the graph. 
+If the j number of the i line is 1, it means that there is an edge from i to j. The i number in line i is always zero.
+
+Print the correct answer in question in adjacency matrix form over a total of N lines. 
+If there is a path from vertex i to j, the jth number on line i should be printed as 1, otherwise 0.
+
+
+ex 1)
