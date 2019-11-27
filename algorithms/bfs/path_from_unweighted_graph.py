0bfa2a9d1678431890fb6f18ae6cb9a973096b76
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2019-11-27 21:51:44 +0900

    [FIX] updated path_from_unweighted_graph.py

--- algorithms/bfs/path_from_unweighted_graph.py
+++ algorithms/bfs/path_from_unweighted_graph.py
@@ -9,3 +9,69 @@ If there is a path from vertex i to j, the jth number on line i should be printe
 
 
 ex 1)
+if input is
+3
+0 1 0
+0 0 1
+1 0 0
+
+output should be
+1 1 1
+1 1 1
+1 1 1
+
+ex 2)
+if input is
+7
+0 0 0 1 0 0 0
+0 0 0 0 0 0 1
+0 0 0 0 0 0 0
+0 0 0 0 1 1 0
+1 0 0 0 0 0 0
+0 0 0 0 0 0 1
+0 0 1 0 0 0 0
+
+output should be
+1 0 1 1 1 1 1
+0 0 1 0 0 0 1
+0 0 0 0 0 0 0
+1 0 1 1 1 1 1
+1 0 1 1 1 1 1
+0 0 1 0 0 0 1
+0 0 1 0 0 0 0
+'''
+
+def bfs(v, d):
+    q = [v]
+    visited = [False] * N
+    state = False
+
+    while q:
+        v = q.pop(0)
+        if not(visited[v]):
+            visited[v] = True
+            for e in adj[v]:
+                if e == d:
+                    print(1, end=" ")
+                    state = True
+                    break
+                if not(visited[e]):
+                    q.append(e)
+            if state:
+                break
+    else:
+        print(0, end=" ")
+
+N = int(input())
+adj = [[] for _ in range(N)]
+
+for i in range(N):
+    a = list(map(int, input().split()))
+    for j in range(len(a)):
+        if a[j]:
+            adj[i].append(j)
+
+for i in range(N):
+    for j in range(N):
+        bfs(i, j)
+    print()
