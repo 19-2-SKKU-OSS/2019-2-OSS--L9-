d9028f2c0243283fcbfd5ad86d0507c941923b2b
NthreeAhn <39453590+NthreeAhn@users.noreply.github.com>
2019-12-08 19:33:05 +0900

    Create test_automata.py

---
+++ tests/test_automata.py
@@ -0,0 +1,22 @@
+from algorithms.automata import DFA
+
+
+import unittest
+
+
+class TestDFA(unittest.TestCase):
+    def test_DFA(self):
+    
+        transitions = {
+            'a': {'1': 'a', '0': 'b'},
+            'b': {'1': 'b', '0': 'a'}
+        }
+
+        final=['a']
+        start = 'a'
+    
+        self.assertEqual(False, DFA(transitions, start, final, "000111100"))
+        self.assertEqual(True, DFA(transitions, start, final, "111000011"))
+
+if __name__ == '__main__':
+    unittest.main()
